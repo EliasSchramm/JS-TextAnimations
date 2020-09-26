f61fc23dec979aad349b7e86e043091c12d4bb9f
EliasSchramm <privat@eps-dev.de>
2020-09-26 22:58:10 +0200

    Update script.js

--- src/basic_textchange/script.js
+++ src/basic_textchange/script.js
@@ -1,15 +1,17 @@
 var text_array = document.getElementsByClassName('animated');
 var timers = [0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var in_hold = [false,false,false,false,false,false,false,false]
 
 const interval = setInterval(function() {
   for (var i = 0; i < text_array.length; i++) {
     	update(text_array[i],i);
-      timers[i] = timers[i] + 100;
+      timers[i] = timers[i] + 20;
   }
-}, 100);
+}, 20);
 
 function update(element, index) {
   let text = element.getAttribute("animated");
+  let hold = parseInt(element.getAttribute("hold"))
   text = text.split(";");
   let animations = text.length;
   let e_index = parseInt(element.getAttribute("index"))
@@ -25,21 +27,43 @@ function update(element, index) {
 
   element.innerHTML = text + "_"
 
-  if(timers[index] >= element.getAttribute("sec")){
+  if(timers[index] >= element.getAttribute("sec") && in_hold[index] != true){
     timers[index] = 0;
     let sub_index = parseInt(element.getAttribute("subindex"))
 
     if(sub_index >= temp.length){
-      sub_index = -1;
+      if(hold == 0){
+        sub_index = -1;
+        element.setAttribute("index",  e_index + 1)
+        if(e_index + 1 >= animations){
+          element.setAttribute("index",  0)
+        }
+      }else {
+        in_hold[index] = true;
+      }
+    }
+
+    if(!(sub_index >= temp.length)){
+      element.setAttribute("subindex",  sub_index + 1)
+    }
+
+  }
+
+  if(in_hold[index]){
+    if(timers[index] >= hold){
+
+      timers[index] = 0;
+      in_hold[index] = 0;
+
+      element.setAttribute("subindex", 0)
+
       element.setAttribute("index",  e_index + 1)
       if(e_index + 1 >= animations){
         element.setAttribute("index",  0)
       }
+
+
     }
-
-    element.setAttribute("subindex",  sub_index + 1)
-
-
   }
 
 
